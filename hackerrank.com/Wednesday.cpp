8a2a74f8d57b952517519e472971b99058ac197d
Richard@MacbookAir <richardlee0211er@gmail.com>
2017-12-14 13:18:25 -0500

    start working on it

--- hackerrank.com/Wednesday.cpp
+++ hackerrank.com/Wednesday.cpp
@@ -1,43 +1,45 @@
+/*
+ * case-insensitive compare
+ */
+
 #include<string>
 #include<iostream>
+#include<ctype.h>
 
 using namespace std;
 
-string foo(){
-    string tmpStr;
-    string str = "Wednesday";
-    int count = 0;
-    cin>>tmpStr;
-    if(tmpStr.length() != 9) return "me too thanks";
-    if(tmpStr[0] == 'W' || tmpStr[0]=='w')
-        count++;
-    if(tmpStr[1] == 'E' || tmpStr[1]=='e')
-        count++;
-    if(tmpStr[2] == 'D' || tmpStr[2]=='d')
-        count++;
-    if(tmpStr[3] == 'N' || tmpStr[3]=='n')
-        count++;
-    if(tmpStr[4] == 'E' || tmpStr[4]=='e')
-        count++;
-    if(tmpStr[5] == 'S' || tmpStr[5]=='s')
-        count++;
-    if(tmpStr[6] == 'D' || tmpStr[6]=='d')
-        count++;
-    if(tmpStr[7] == 'A' || tmpStr[7]=='a')
-        count++;
-    if(tmpStr[8] == 'Y' || tmpStr[8]=='y')
-        count++;
-    if(count == 9)
-        return "It is Wednesday, my dudes";
-
-    else{
-
-        return "me too thanks";
-    }
+bool is_letter(char a)
+{
+    if((a>='A'&&a<='Z') || (a>='a'&&a<='z'))
+        return true;
+    return false;
 }
 
-int main(){
+bool case_insensitive_compare(string str1, string str2)
+{
+    if(str1.length() != str2.length()) return false;
 
-    cout<<foo();
+    for(int i=0; i<(int)str1.length(); ++i){
+        if(is_letter(str1[i])){
+            if(toupper(str1[i]) != toupper(str1[i]))
+                return false;
+        }else{
+            if(str1[i] != str2[i])
+                return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    string str1 = "wendesday";
+    string str2 = "Wendesday";
+    cin>>str2;
+    if(case_insensitive_compare(str1, str2))
+        cout<<"It is Wednesday, my dudes\n";
+    else
+        cout<<"me too thanks\n";
+
     return 0;
 }
